8d53b92f94f9f75bd1b6048f4f983332f44666cd
drew <drew.e.westphal@gmail.com>
2014-07-23 12:33:06 -0400

    Add actual reverse proxy SSL support by inserting line before the rest of wordpress is loaded. Per http://codex.wordpress.org/Function_Reference/is_ssl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -43,14 +43,14 @@ fi
 
 if [ ! -e wp-config.php ]; then
 	cp wp-config-sample.php wp-config.php
-	cat >> wp-config.php <<'EOPHP'
-
-// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
-EOPHP
+	sed -i -e "/define('WP_DEBUG'/r /dev/stdin" wp-config.php <<- 'EOPHP'
+	
+	// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+	// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+	if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+		$_SERVER['HTTPS'] = 'on';
+	}
+	EOPHP
 fi
 
 set_config() {
