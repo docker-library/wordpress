c0e9bc5a73f06773910ef3bba2da9aa7df044415
Tianon Gravi <admwiggin@gmail.com>
2014-08-11 15:32:04 -0600

    Initial version without upstream artifacts

---
+++ Dockerfile
@@ -0,0 +1,50 @@
+FROM debian:wheezy
+
+RUN apt-get update && apt-get install -y \
+		apache2 \
+		curl \
+		libapache2-mod-php5 \
+		php5-curl \
+		php5-gd \
+		php5-mysql \
+		rsync \
+		wget
+RUN a2enmod rewrite
+
+# copy a few things from apache's init script that it requires to be setup
+ENV APACHE_CONFDIR /etc/apache2
+ENV APACHE_ENVVARS $APACHE_CONFDIR/envvars
+# and then a few more from $APACHE_CONFDIR/envvars itself
+ENV APACHE_RUN_USER www-data
+ENV APACHE_RUN_GROUP www-data
+ENV APACHE_RUN_DIR /var/run/apache2
+ENV APACHE_PID_FILE $APACHE_RUN_DIR/apache2.pid
+ENV APACHE_LOCK_DIR /var/lock/apache2
+ENV APACHE_LOG_DIR /var/log/apache2
+ENV LANG C
+RUN mkdir -p $APACHE_RUN_DIR $APACHE_LOCK_DIR $APACHE_LOG_DIR
+
+# make CustomLog (access log) go to stdout instead of files
+#  and ErrorLog to stderr
+RUN find "$APACHE_CONFDIR" -type f -exec sed -ri ' \
+	s!^(\s*CustomLog)\s+\S+!\1 /proc/self/fd/1!g; \
+	s!^(\s*ErrorLog)\s+\S+!\1 /proc/self/fd/2!g; \
+' '{}' ';'
+
+RUN rm -rf /var/www/html && mkdir /var/www/html
+VOLUME /var/www/html
+WORKDIR /var/www/html
+
+ENV WORDPRESS_VERSION 3.9.2
+
+# upstream tarballs include ./wordpress/ so this gives us /usr/src/wordpress
+RUN curl -SL http://wordpress.org/wordpress-$WORDPRESS_VERSION.tar.gz | tar -xzC /usr/src/
+
+COPY docker-apache.conf /etc/apache2/sites-available/wordpress
+RUN a2dissite 000-default && a2ensite wordpress
+
+COPY docker-entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+EXPOSE 80
+CMD ["apache2", "-DFOREGROUND"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+set -e
+
+if [ -z "$MYSQL_PORT_3306_TCP" ]; then
+	echo >&2 'error: missing MYSQL_PORT_3306_TCP environment variable'
+	echo >&2 '  Did you forget to --link some_mysql_container:mysql ?'
+	exit 1
+fi
+
+# if we're linked to MySQL, and we're using the root user, and our linked
+# container has a default "root" password set up and passed through... :)
+: ${WORDPRESS_DB_USER:=root}
+if [ "$WORDPRESS_DB_USER" = 'root' ]; then
+	: ${WORDPRESS_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
+fi
+: ${WORDPRESS_DB_NAME:=wordpress}
+
+if [ -z "$WORDPRESS_DB_PASSWORD" ]; then
+	echo >&2 'error: missing required WORDPRESS_DB_PASSWORD environment variable'
+	echo >&2 '  Did you forget to -e WORDPRESS_DB_PASSWORD=... ?'
+	echo >&2
+	echo >&2 '  (Also of interest might be WORDPRESS_DB_USER and WORDPRESS_DB_NAME.)'
+	exit 1
+fi
+
+if ! [ -e index.php -a -e wp-includes/version.php ]; then
+	echo >&2 "WordPress not found in $(pwd) - copying now..."
+	if [ "$(ls -A)" ]; then
+		echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
+		( set -x; ls -A; sleep 10 )
+	fi
+	rsync --archive --one-file-system --quiet /usr/src/wordpress/ ./
+	echo >&2 "Complete! WordPress has been successfully copied to $(pwd)"
+	if [ ! -e .htaccess ]; then
+		cat > .htaccess <<-'EOF'
+			RewriteEngine On
+			RewriteBase /
+			RewriteRule ^index\.php$ - [L]
+			RewriteCond %{REQUEST_FILENAME} !-f
+			RewriteCond %{REQUEST_FILENAME} !-d
+			RewriteRule . /index.php [L]
+		EOF
+	fi
+fi
+
+# TODO handle WordPress upgrades magically in the same way, but only if wp-includes/version.php's $wp_version is less than /usr/src/wordpress/wp-includes/version.php's $wp_version
+
+if [ ! -e wp-config.php ]; then
+	awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+EOPHP
+fi
+
+set_config() {
+	key="$1"
+	value="$2"
+	php_escaped_value="$(php -r 'var_export($argv[1]);' "$value")"
+	sed_escaped_value="$(echo "$php_escaped_value" | sed 's/[\/&]/\\&/g')"
+	sed -ri "s/((['\"])$key\2\s*,\s*)(['\"]).*\3/\1$sed_escaped_value/" wp-config.php
+}
+
+WORDPRESS_DB_HOST="${MYSQL_PORT_3306_TCP#tcp://}"
+
+set_config 'DB_HOST' "$WORDPRESS_DB_HOST"
+set_config 'DB_USER' "$WORDPRESS_DB_USER"
+set_config 'DB_PASSWORD' "$WORDPRESS_DB_PASSWORD"
+set_config 'DB_NAME' "$WORDPRESS_DB_NAME"
+
+# allow any of these "Authentication Unique Keys and Salts." to be specified via
+# environment variables with a "WORDPRESS_" prefix (ie, "WORDPRESS_AUTH_KEY")
+UNIQUES=(
+	AUTH_KEY
+	SECURE_AUTH_KEY
+	LOGGED_IN_KEY
+	NONCE_KEY
+	AUTH_SALT
+	SECURE_AUTH_SALT
+	LOGGED_IN_SALT
+	NONCE_SALT
+)
+for unique in "${UNIQUES[@]}"; do
+	eval unique_value=\$WORDPRESS_$unique
+	if [ "$unique_value" ]; then
+		set_config "$unique" "$unique_value"
+	else
+		# if not specified, let's generate a random value
+		set_config "$unique" "$(head -c1M /dev/urandom | sha1sum | cut -d' ' -f1)"
+	fi
+done
+
+TERM=dumb php -- "$WORDPRESS_DB_HOST" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" <<'EOPHP'
+<?php
+// database might not exist, so let's try creating it (just to be safe)
+
+list($host, $port) = explode(':', $argv[1], 2);
+$mysql = new mysqli($host, $argv[2], $argv[3], '', (int)$port);
+
+if ($mysql->connect_error) {
+	file_put_contents('php://stderr', 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+	exit(1);
+}
+
+if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($argv[4]) . '`')) {
+	file_put_contents('php://stderr', 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
+	$mysql->close();
+	exit(1);
+}
+
+$mysql->close();
+EOPHP
+
+chown -R "$APACHE_RUN_USER:$APACHE_RUN_GROUP" .
+
+exec "$@"
