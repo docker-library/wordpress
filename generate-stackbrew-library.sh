6210848ab3f761d5f3bafbb9cad4495807d26a0a
Tianon Gravi <admwiggin@gmail.com>
2014-08-11 16:20:20 -0600

    Add very simple generate-stackbrew-library.sh

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+url='git://github.com/docker-library/wordpress'
+
+echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+
+commit="$(git log -1 --format='format:%H' .)"
+fullVersion="$(grep -m1 'ENV WORDPRESS_VERSION ' Dockerfile | cut -d' ' -f3)"
+
+versionAliases=()
+while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+	versionAliases+=( $fullVersion )
+	fullVersion="${fullVersion%.*}"
+done
+versionAliases+=( $fullVersion latest )
+
+echo
+for va in "${versionAliases[@]}"; do
+	echo "$va: ${url}@${commit} $version"
+done
