c045414de532631ff95b940fa4f513d068931005
Tianon Gravi <admwiggin@gmail.com>
2014-09-04 14:27:24 -0600

    Change update.sh to munge "4.0" into "4.0.0" so we keep the "4.0.0" tag when "4.0.1" comes around

--- update.sh
+++ update.sh
@@ -3,4 +3,10 @@ set -e
 
 current="$(curl -sSL 'http://api.wordpress.org/core/version-check/1.7/' | sed -r 's/^.*"current":"([^"]+)".*$/\1/')"
 
-sed -ri 's/^(ENV WORDPRESS_VERSION ).*/\1'"$current"'/' Dockerfile
+if [[ "$current" != *.*.* ]]; then
+	# turn "4.0" into "4.0.0"
+	current+='.0'
+fi
+
+set -x
+sed -ri 's/^(ENV WORDPRESS_VERSION) .*/\1 '"$current"'/' Dockerfile
