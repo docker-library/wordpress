bf4309b4dc053db77349bca67e03669ba9d09ae4
Tianon Gravi <admwiggin@gmail.com>
2014-12-31 22:28:12 -0700

    Protect against branches named "." in generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -7,7 +7,7 @@ url='git://github.com/docker-library/wordpress'
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
-commit="$(git log -1 --format='format:%H' .)"
+commit="$(git log -1 --format='format:%H' -- .)"
 fullVersion="$(grep -m1 'ENV WORDPRESS_VERSION ' Dockerfile | cut -d' ' -f3)"
 
 versionAliases=()
