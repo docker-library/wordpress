5fa7fa7384d70bfda395392a5ada6206dd2fcae8
Mike Dillon <mike@embody.org>
2015-02-14 09:31:17 -0800

    Update generate-stackbrew-library.sh for multiple variants
    Still need to get it to generated unsuffixed tags for the 'apache' variant
    along with a 'latest' tag

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -7,17 +7,19 @@ url='git://github.com/docker-library/wordpress'
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
-commit="$(git log -1 --format='format:%H' -- .)"
-fullVersion="$(grep -m1 'ENV WORDPRESS_VERSION ' Dockerfile | cut -d' ' -f3)"
+for variant in apache fpm; do
+	commit="$(git log -1 --format='format:%H' -- "$variant")"
+	fullVersion="$(grep -m1 'ENV WORDPRESS_VERSION ' "$variant/Dockerfile" | cut -d' ' -f3)"
 
-versionAliases=()
-while [ "${fullVersion%.*}" != "$fullVersion" ]; do
-	versionAliases+=( $fullVersion )
-	fullVersion="${fullVersion%.*}"
-done
-versionAliases+=( $fullVersion latest )
+	versionAliases=()
+	while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+		versionAliases+=( $fullVersion-$variant )
+		fullVersion="${fullVersion%.*}"
+	done
+	versionAliases+=( $fullVersion-$variant $variant )
 
-echo
-for va in "${versionAliases[@]}"; do
-	echo "$va: ${url}@${commit}"
+	echo
+	for va in "${versionAliases[@]}"; do
+		echo "$va: ${url}@${commit} $variant"
+	done
 done
