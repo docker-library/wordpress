aae7350f8312c7860793b4f515703a9d833b127d
Mike Dillon <mike@embody.org>
2015-02-14 10:13:26 -0800

    Add unsuffixed tags for apache variant

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -7,6 +7,8 @@ url='git://github.com/docker-library/wordpress'
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
+defaultVariant='apache'
+
 for variant in apache fpm; do
 	commit="$(git log -1 --format='format:%H' -- "$variant")"
 	fullVersion="$(grep -m1 'ENV WORDPRESS_VERSION ' "$variant/Dockerfile" | cut -d' ' -f3)"
@@ -14,9 +16,15 @@ for variant in apache fpm; do
 	versionAliases=()
 	while [ "${fullVersion%.*}" != "$fullVersion" ]; do
 		versionAliases+=( $fullVersion-$variant )
+		if [ "$variant" = "$defaultVariant" ]; then
+			versionAliases+=( $fullVersion )
+		fi
 		fullVersion="${fullVersion%.*}"
 	done
 	versionAliases+=( $fullVersion-$variant $variant )
+	if [ "$variant" = "$defaultVariant" ]; then
+		versionAliases+=( $fullVersion latest )
+	fi
 
 	echo
 	for va in "${versionAliases[@]}"; do
