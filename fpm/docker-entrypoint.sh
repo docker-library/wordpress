862d97aa4c6c7e879b366f53dd853212adccedef
Patrick Puritscher <blackout289@gmail.com>
2015-03-08 19:02:10 +0100

    added wpcomments to .htaccess

--- fpm/docker-entrypoint.sh
+++ fpm/docker-entrypoint.sh
@@ -45,12 +45,16 @@ if ! [ -e index.php -a -e wp-includes/version.php ]; then
 	if [ ! -e .htaccess ]; then
 		# NOTE: The "Indexes" option is disabled in the php:apache base image
 		cat > .htaccess <<-'EOF'
+			# BEGIN WordPress
+			<IfModule mod_rewrite.c>
 			RewriteEngine On
 			RewriteBase /
 			RewriteRule ^index\.php$ - [L]
 			RewriteCond %{REQUEST_FILENAME} !-f
 			RewriteCond %{REQUEST_FILENAME} !-d
 			RewriteRule . /index.php [L]
+			</IfModule>
+			# END WordPress
 		EOF
 	fi
 fi
