75b7704a63108f5f64858f5c4c0be02ca91432ed
Tianon Gravi <admwiggin@gmail.com>
2015-08-04 10:18:12 -0700

    Move the "funky" version handling into "generate-stackbrew-library.sh" where it belongs (instead of "update.sh")

--- update.sh
+++ update.sh
@@ -3,13 +3,7 @@ set -e
 
 current="$(curl -sSL 'http://api.wordpress.org/core/version-check/1.7/' | sed -r 's/^.*"current":"([^"]+)".*$/\1/')"
 
-upstream="$current"
-if [[ "$current" != *.*.* ]]; then
-	# turn "4.0" into "4.0.0"
-	current+='.0'
-fi
-
-sha1="$(curl -sSL "https://wordpress.org/wordpress-$upstream.tar.gz.sha1")"
+sha1="$(curl -sSL "https://wordpress.org/wordpress-$current.tar.gz.sha1")"
 
 for variant in apache fpm; do
 	(
@@ -17,7 +11,6 @@ for variant in apache fpm; do
 
 		sed -ri '
 			s/^(ENV WORDPRESS_VERSION) .*/\1 '"$current"'/;
-			s/^(ENV WORDPRESS_UPSTREAM_VERSION) .*/\1 '"$upstream"'/;
 			s/^(ENV WORDPRESS_SHA1) .*/\1 '"$sha1"'/;
 		' "$variant/Dockerfile"
 
