459f2a73b022b9f5cd0c3df6ced2a9c03dbc8378
Tianon Gravi <admwiggin@gmail.com>
2015-08-12 13:54:11 -0700

    Add initial .travis.yml

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 current="$(curl -sSL 'http://api.wordpress.org/core/version-check/1.7/' | sed -r 's/^.*"current":"([^"]+)".*$/\1/')"
 
 sha1="$(curl -sSL "https://wordpress.org/wordpress-$current.tar.gz.sha1")"
 
+travisEnv=
 for variant in apache fpm; do
 	(
 		set -x
@@ -16,4 +17,9 @@ for variant in apache fpm; do
 
 		cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
 	)
+	
+	travisEnv+='\n  - VARIANT='"$variant"
 done
+
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
