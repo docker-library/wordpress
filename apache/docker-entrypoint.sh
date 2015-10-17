4fa2a43574e7aa81eb0fea2ef8a2f5535612a9b5
Oded Arbel <oded@geek.co.il>
2015-10-18 01:36:28 +0300

    use wordpress recommended rewrite setup

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -51,9 +51,16 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 				RewriteEngine On
 				RewriteBase /
 				RewriteRule ^index\.php$ - [L]
-				RewriteCond %{REQUEST_FILENAME} !-f
-				RewriteCond %{REQUEST_FILENAME} !-d
-				RewriteRule . /index.php [L]
+				
+				# add a trailing slash to /wp-admin
+				RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]
+				
+				RewriteCond %{REQUEST_FILENAME} -f [OR]
+				RewriteCond %{REQUEST_FILENAME} -d
+				RewriteRule ^ - [L]
+				RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]
+				RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]
+				RewriteRule . index.php [L]
 				</IfModule>
 				# END WordPress
 			EOF
