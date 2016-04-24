cb3918f57f455778be02218b7116350ead505add
Leonhardt Wille <hallo@leonhardt-wille.de>
2016-04-24 20:44:21 +0200

    feat(config): evaluate extra WORDPRESS_ADDITIONAL_CONFIG env var
    Contents of WORDPRESS_ADDITIONAL_CONFIG will be directly injected to wp-config.php.

--- php7.2/fpm/docker-entrypoint.sh
+++ php7.2/fpm/docker-entrypoint.sh
@@ -128,14 +128,15 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 		sed -ri -e 's/\r$//' wp-config*
 
 		if [ ! -e wp-config.php ]; then
-			awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
+			awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP' <<EOBASH
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
 	$_SERVER['HTTPS'] = 'on';
 }
-
 EOPHP
+${WORDPRESS_ADDITIONAL_CONFIG:-}
+EOBASH
 			chown "$user:$group" wp-config.php
 		fi
 
