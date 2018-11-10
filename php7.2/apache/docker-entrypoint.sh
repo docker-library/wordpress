8492e39cbdd88b81cad23407300ac98ce3a13136
Blue Gaston <bbluegaston@gmail.com>
2018-11-10 08:09:00 -0800

    precompile php to reduce compile time at scramble

--- php7.2/apache/docker-entrypoint.sh
+++ php7.2/apache/docker-entrypoint.sh
@@ -46,6 +46,7 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 			echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
 			( set -x; ls -A; sleep 10 )
 		fi
+		scramble.sh
 		tar --create \
 			--file - \
 			--one-file-system \
