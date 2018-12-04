1e4be503cbc7cc3ee024c2f0659a019e23b1c0b4
Archis Gore <archis@polyverse.io>
2018-12-04 11:44:34 -0800

    Temporary

---
+++ php7.2/apache/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "$(date) Obtaining current git sha for tagging the docker image"
+headsha=$(git rev-parse --verify HEAD)
+
+docker run --name mysql-host -e MYSQL_ROOT_PASSWORD=qwerty -d mysql:5.7
+docker run --rm -e MODE=$MODE --name wordpress -v $PWD/wordpress:/wordpress  --link mysql-host:mysql -p 8000:80  polyverse/polyscripted-wordpress:$headsha
---
+++ php7.2/apache/scramble.sh
@@ -0,0 +1,31 @@
+if [ ! -f "$PATH/s_php" ]; then
+        cp /usr/local/bin/php /usr/local/bin/s_php
+fi
+
+
+if [ "$MODE" == "polyscripted" ] || [ -e /polyscripted ]; then
+	
+	if [ -f /wordpress ]; then
+		cp /wordpress /usr/src/wordpress/
+	fi
+
+	echo "Starting polyscripted WordPress"
+	cd $POLYSCRIPT_PATH
+	sed -i "/#mod_allow/a \define( 'DISALLOW_FILE_MODS', true );" /usr/src/wordpress/wp-config.php
+    	./build-scrambled.sh
+	if [ -f scrambled.json ] && s_php tok-php-transformer.php -p /usr/src/wordpress --replace; then
+		echo "Polyscripting enabled."
+		echo "done"
+	else
+		echo "Polyscripting failed."
+		cp /usr/local/bin/s_php /usr/local/bin/php
+		exit 1
+	fi
+else
+    echo "Polyscripted mode is off. To enable it, either:"
+    echo "  1. Set the environment variable: MODE=polyscripted"
+    echo "  2. OR create a file at path: /polyscripted"
+
+    # Symlink the mount so it's editable
+    ln -s /wordpress /var/www/html
+fi
