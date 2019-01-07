2c976a02b70c23b4e935a3053c14e0ca86261925
Blue Gaston <bbluegaston@gmail.com>
2019-01-06 21:05:35 -0800

    fixed publish image naming

--- php7.2/fpm-alpine/publish-image.sh
+++ php7.2/fpm-alpine/publish-image.sh
@@ -7,11 +7,11 @@ headsha=$(git rev-parse --verify HEAD)
 
 
 docker build -t $image:alpine-$headsha .
-docker tag $image:alpine-$headsha $image:alpine
+docker tag $image:alpine-$headsha $image:alpine-latest
 docker tag $image:alpine-$headsha $image:latest
 
 if [[ "$1" == "-p" ]]; then
-	docker push $image:$headsha
-	docker push $image:alpine
+	docker push $image:alpine-$headsha
+	docker push $image:alpine-latest
 	docker push $image:latest
 fi
