9deba43648d6bc0a4b155c0fd2c91ce01293585a
Blue Gaston <bbluegaston@gmail.com>
2019-01-07 10:29:12 -0800

    modified run

--- php7.2/apache/run.sh
+++ php7.2/apache/run.sh
@@ -8,4 +8,4 @@ echo "$(date) Obtaining current git sha for tagging the docker image"
 headsha=$(git rev-parse --verify HEAD)
 
 docker run --name mysql-host -e MYSQL_ROOT_PASSWORD=qwerty -d mysql:5.7
-docker run --rm -e MODE=$MODE --name wordpress -v $PWD/wordpress:/wordpress  --link mysql-host:mysql -p 8000:80  polyverse/polyscripted-wordpress:$headsha
+docker run --rm -e MODE=$MODE --name wordpress -v $PWD/wordpress:/wordpress  --link mysql-host:mysql -p 8000:80  polyverse/polyscripted-wordpress:debian-$headsha
