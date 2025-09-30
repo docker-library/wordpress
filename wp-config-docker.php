e3d20f697461a957107a19f4bde401704f7d6fe6
suedauckun-arcaedia <sueda@arcaedia.com>
2025-09-30 15:07:56 +0300

    Update wp-config-docker.php

--- wp-config-docker.php
+++ wp-config-docker.php
@@ -134,6 +134,8 @@ if ($configExtra = getenv_docker('WORDPRESS_CONFIG_EXTRA', '')) {
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
+define('WP_ALLOW_MULTISITE', true);
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
