ce303ebfe1bd994b8ad54c8ce34686579ffdce41
Tianon Gravi <admwiggin@gmail.com>
2025-09-30 10:45:54 -0700

    Fix "unbound variable" error in `docker-ensure-installed.sh`

--- latest/php8.4/fpm/docker-entrypoint.sh
+++ latest/php8.4/fpm/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-if [[ "$1" == apache2* ]] || [ "$1" = 'php-fpm' ] || { self="$(basename "$0")" && [ "$self" = 'docker-ensure-installed.sh' ]; }; then
+if [[ "${1-}" == apache2* ]] || [ "${1-}" = 'php-fpm' ] || { self="$(basename "$0")" && [ "$self" = 'docker-ensure-installed.sh' ]; }; then
 	uid="$(id -u)"
 	gid="$(id -g)"
 	if [ "$uid" = '0' ]; then
-		case "$1" in
+		case "${1-}" in
 			apache2*)
 				user="${APACHE_RUN_USER:-www-data}"
 				group="${APACHE_RUN_GROUP:-www-data}"
